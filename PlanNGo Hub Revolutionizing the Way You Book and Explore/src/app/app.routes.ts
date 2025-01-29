3c82abffdd962047959992f52b89f047640aa115
Guddu Kumar <guddu748081@gmail.com>
2025-01-29 23:55:01 +0530

    updated code  with final Sprint-4

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
@@ -4,15 +4,18 @@ import { RegisterComponent } from './authentication/components/register/register
 import { HomeComponent } from './authentication/components/home/home.component';
 import { authGuard } from './authentication/services/auth.guard';
 import { ForgotPasswordComponent } from './authentication/components/forgotpassword/forgotpassword.component';
-import { RouterModule  } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AdminDashboardComponent } from './authentication/components/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './authentication/components/user-dashboard/user-dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-  { path: 'admin-dashboard', component: AdminDashboardComponent }, 
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
   { path: 'forgotpassword', component: ForgotPasswordComponent },
+  { path: 'user-dashboard', component: UserDashboardComponent },
+
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
   { path: '**', redirectTo: '/login' }, // Fallback route for unmatched paths
 ];
