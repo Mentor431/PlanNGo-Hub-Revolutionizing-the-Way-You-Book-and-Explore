c33dd24a6a8b763b973b828fb3ef7b741d81b1ce
Noorul Ashfaq A <noorulashfaq786@gmail.com>
2025-01-30 14:01:38 +0530

    Integrated Team 1 with the app

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/tailwind.config.js
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
     "./src/app/tour/component/**/*.{html,ts}",
+    "./src/app/authentication/component/**/*.{html,ts}",
     "./public/index.html",
   ],
   theme: {
