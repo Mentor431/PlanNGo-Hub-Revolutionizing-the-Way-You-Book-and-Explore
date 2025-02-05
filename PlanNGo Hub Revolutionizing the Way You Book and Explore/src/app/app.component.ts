0523041a836eceabfc576b75187a3b33d4db2e03
Noorul Ashfaq A <noorulashfaq786@gmail.com>
2025-02-05 16:51:54 +0530

    navbar, favicon & logo fixed

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.component.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.component.ts
@@ -4,14 +4,26 @@ import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { faHotel } from '@fortawesome/free-solid-svg-icons';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faSuitcase } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, ToastModule],
+  imports: [RouterOutlet, FontAwesomeModule, ButtonModule, ToastModule],
   providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = "PlanNGo-Hub";
+
+  faCar = faCar;
+  faHotel = faHotel;
+  faPlane = faPlane;
+  faSuitcase = faSuitcase;
+
 }
